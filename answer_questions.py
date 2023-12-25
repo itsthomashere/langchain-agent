9e08b376fb44230d3e1f8491f88473bf0e27c7dd
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-25 12:10:09 +0200

    Add answer_questions.py file

---
+++ answer_questions.py
@@ -0,0 +1,12 @@
+from llama_index import StorageContext, load_index_from_storage
+
+# load knowledge base from disk.
+index = load_index_from_storage(
+  StorageContext.from_defaults(persist_dir="storage"))
+
+# make the knowledge base into a query engine—an object that queries can be run on
+query_engine = index.as_query_engine()
+
+def answer_question(query):
+    """Run a query on the query engine."""
+    return query_engine.query(query)
