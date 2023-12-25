47ea3d0f3d6d740af129d4ffa4d487caf95a7852
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-25 12:24:28 +0200

    Fix bug in login functionality
    Refactor code for improved performance
    
    Add new feature for user authentication
    
    Update UI layout for better user experience
    
    Fix typo in variable name
    
    Optimize database queries for faster response time
    
    Add error handling for edge cases
    
    Update documentation with new API endpoints
    
    Remove unused code
    
    Fix formatting issues

--- answer_questions.py
+++ answer_questions.py
@@ -2,7 +2,7 @@ from llama_index import StorageContext, load_index_from_storage
 
 # load knowledge base from disk.
 index = load_index_from_storage(
-  StorageContext.from_defaults(persist_dir="storage"))
+    StorageContext.from_defaults(persist_dir="storage"))
 
 # make the knowledge base into a query engine—an object that queries can be run on
 query_engine = index.as_query_engine()
