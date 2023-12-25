b273582bc49d90db01a42fde828cdec502e6db15
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-25 15:56:56 +0200

    Modifying citation string

--- answer_questions.py
+++ answer_questions.py
@@ -25,15 +25,14 @@ def answer_question(query):
     retrieved_nodes = retriever.retrieve(query)
     response = query_engine.query(query)
     response_text = response.response
+    reference_list = []
     for i, node in enumerate(response.source_nodes):
 
         title = f"\n**Source {i+1}** {node.node.metadata.get('title')}"
         page = f"**Page** {node.node.metadata.get('page_number')}"
         percentage = f"**Relevance** {node.score * 100:.1f}%"
+        reference_list.append(f"{title}\n{page}\n{percentage}")
 
         st.toast(f"{title}\n{page}\n{percentage}", icon="ℹ️")
-        st.markdown(f"**{title}**")
-        st.markdown(f"**page {page}**")
-        st.markdown(f"**{percentage}% relevance**")
 
-    return response_text # + "\n\n" + f"{title}\n{page}\n{percentage}"
+    return response_text + "\n\n" + "\n".join(reference_list)
