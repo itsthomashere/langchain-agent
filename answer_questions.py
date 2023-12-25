ca4315907ef38dbeebe32b90fe0ac39ce834fbb3
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-25 15:54:36 +0200

    Modifying citation string

--- answer_questions.py
+++ answer_questions.py
@@ -23,25 +23,17 @@ similarity_top_k=5  # Modify this value to change top K retrievals
 def answer_question(query):
     """Run a query on the query engine."""
     retrieved_nodes = retriever.retrieve(query)
-    # st.write(retrieved_nodes)
     response = query_engine.query(query)
-    # st.write(response)
     response_text = response.response
-    # st.write(response_text)
-    # st.write(response.source_nodes[0].get_content())
     for i, node in enumerate(response.source_nodes):
-    #     print("-----")
-    #     text_fmt = node.node.get_content().strip().replace("\n", " ")[:1000]
-        # st.write(f"Text:\t {text_fmt} ...")
-        # st.write(f"Metadata:\t {node.node.metadata}")
-    #     # print out the page number and the metadata
-        # st.info('This is a purely informational message', icon="ℹ️")
 
         title = f"\n**Source {i+1}** {node.node.metadata.get('title')}"
         page = f"**Page** {node.node.metadata.get('page_number')}"
         percentage = f"**Relevance** {node.score * 100:.1f}%"
 
         st.toast(f"{title}\n{page}\n{percentage}", icon="ℹ️")
-    # # print(response.get_formatted_sources())
+        st.markdown(f"**{title}**")
+        st.markdown(f"**page {page}**")
+        st.markdown(f"**{percentage}% relevance**")
 
-    return response_text + "\n\n" + f"{title}\n{page}\n{percentage}"
+    return response_text # + "\n\n" + f"{title}\n{page}\n{percentage}"
