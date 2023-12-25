58fd34a49bd2db797b1f102a9610eac646aa74a6
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-25 14:33:18 +0200

    Refactor response_text assignment in answer_question function

--- answer_questions.py
+++ answer_questions.py
@@ -26,7 +26,7 @@ def answer_question(query):
     # st.write(retrieved_nodes)
     response = query_engine.query(query)
     st.write(response)
-    response_text = response.get_response().response
+    response_text = response.response
     st.write(response_text)
     # st.write(response.source_nodes[0].get_content())
     for i, node in enumerate(response.source_nodes):
